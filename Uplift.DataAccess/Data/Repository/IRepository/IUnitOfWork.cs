9ffed06d525fee163d9ed97a1c5263a0d2fc8ec6
Kedar Nayak <kedarn@softura.com>
2020-05-14 07:16:30 +0530

    Add project files.

---
+++ Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        IFrequencyRepository Frequency { get; }
+        IServiceRepository Service { get; }
+        void Save();
+        IOrderHeaderRepository OrderHeader { get; }
+        IOrderDetailsRepository OrderDetails { get; }
+        IUserRepository User { get; }
+        ISP_Call SP_Call { get; }
+    }
+}
---
+++ Uplift.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+        public string DisplayOrder { get; set; }
+    }
+}
